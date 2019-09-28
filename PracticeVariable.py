951e73fdd6fb444f293cf2ba8e1afb516d97e287
pbhuti2 <55327021+pbhuti2@users.noreply.github.com>
2019-09-28 11:34:14 -0500

    Initial commit

---
+++ PracticeVariable.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+a = 4
+
+b = 3.45
+
+c = "string"
+
+a = True
+
+list = [3, 4, 5]
+
+listTwo = ["3", "4", "5"]
+
+listThree = [True, False, False]
